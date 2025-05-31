f07f26e6fe84e4bc27fa2d9c79d1c0f7ab58b678
deathbyprograms <24487592+deathbyprograms@users.noreply.github.com>
2025-05-31 14:47:45 -0600

    Add jank fix

--- src/utils/channel.js
+++ src/utils/channel.js
@@ -1,13 +1,22 @@
 let currentChannel;
 
 export function setCurrentChannel({provider, id, name, displayName, avatar}) {
-  currentChannel = {
-    provider,
-    id: id.toString(),
-    name,
-    displayName,
-    avatar,
-  };
+  if (provider === 'youtube' && id.toString() === 'UC2I6ta1bWX7DnEuYNvHiptQ') {
+    currentChannel = {
+      provider: 'twitch',
+      id: '85498365',
+      name: 'vedal987',
+      displayName: 'vedal987',
+    };
+  } else {
+    currentChannel = {
+      provider,
+      id: id.toString(),
+      name,
+      displayName,
+      avatar,
+    };
+  }
 }
 
 export function getCurrentChannel() {
